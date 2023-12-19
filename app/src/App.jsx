3e50095b9ecfd020baeced397e6488d91a39549e
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-19 19:36:37 +0500

    Background Added in this !

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,21 @@
-import Header from "./components/header/Header"
-import Hero from "./components/hero/hero"
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero.jsx";
+import "./App.css";
+import Companies from "./components/Companies/Companies.jsx";
+import Residencies from "./components/Residencies/Residencies.jsx";
 
 const App = () => {
   return (
     <div className="App">
-
-      <Header />
-      <Hero />
+      <div>
+      <div className="white-gradient"/>
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
