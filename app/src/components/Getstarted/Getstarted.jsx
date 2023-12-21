a42efdc6808f2d881316e7e791b26372ec10e40f
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-21 18:16:19 +0500

    Get Started Page Added!

---
+++ app/src/components/Getstarted/Getstarted.jsx
@@ -0,0 +1,21 @@
+import "./Getstarted.css";
+
+const Getstarted = () => {
+  return (
+    <section className="g-wrapper">
+      <div className=" paddings innerWdith g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get started with Homyz</span>
+          <span className="secondaryText">Subscribe and find super attractive price quotes from us. <br />
+Find your residence soon</span>
+
+<button className="button">
+    <a href="mailto:syedziashahgillani76gmail.com">Get Started</a>
+</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Getstarted;
